8cafe8e426bd21d6707980b378cb810e5d5213ee
Kumar Gaurav <kmrgaurav33@gmail.com>
2025-12-22 20:37:13 +0530

    lc: random array probelm

---
+++ js/random.js
@@ -0,0 +1,13 @@
+const input = [12,345,2,6,7896]
+
+
+const findNumbers = function (nums) {
+  let result = 0
+  nums.forEach(x => {
+    const noOfDigits = Math.ceil(Math.log10(x))
+    if (noOfDigits % 2 === 0) result++
+  })
+  return result
+}
+
+console.log(findNumbers(input))
