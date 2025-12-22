3910c776e0729c7db082fe3981140a6366ecf31e
Kumar Gaurav <kmrgaurav33@gmail.com>
2025-12-22 20:57:46 +0530

    lc: 977.js with different approach

--- js/977.js
+++ js/977.js
@@ -3,24 +3,8 @@
  * @return {number[]}
  */
 const sortedSquares = function(nums) {
-  let left = 0, right = nums.length - 1
-  const result = []
-
-  for (; left <= right; ) {
-    const leftNum = nums[left]
-    const leftSquare = leftNum * leftNum
-    const rightNum = nums[right]
-    const rightSquare = rightNum * rightNum
-    
-    if (leftSquare > rightSquare) {
-      result.unshift(leftSquare)
-      left++
-    } else {
-      result.unshift(rightSquare)
-      right--
-    }
-  }
-  return result
+  const squared = nums.map(x => x * x)
+  return squared.sort((a, b) => a - b)
 };
 
 
